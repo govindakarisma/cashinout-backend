ffa9f62b96facfa7e26ff257791b841628454670
govindakarisma <govindakharisma10@gmail.com>
2023-03-09 21:17:58 +0700

    setup table dan relasinya

---
+++ app/Models/Cash.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Cash extends Model
+{
+    use HasFactory;
+    protected $guarded = ["id"];
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();;
+        });
+    }
+}
--- database/migrations/2023_03_09_210439_create_cashes_table.php
+++ database/migrations/2023_03_09_210439_create_cashes_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cashes', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->foreignId('user_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('slug');
+            $table->string('description')->nullable();
+            $table->double('amount');
+            $table->timestamp('when');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cashes');
     }
 };
